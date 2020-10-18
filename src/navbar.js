c71d8c6577f1a610fde7037548669b52411336db
PALLAVI  DHAR <66560935+PallaviDhar@users.noreply.github.com>
2020-10-18 11:37:45 +0530

    React

--- src/navbar.js
+++ src/navbar.js
@@ -4,7 +4,7 @@ class Navbar extends Component {
     state = {  }
     render() {
         return (
-            <div className="navbar bg-primary stricky-top">
+            <div className="navbar bg-primary sticky-top">
                 <a href="#" className="navbar-brand text-white">Flipkart</a>
             </div>
         );
